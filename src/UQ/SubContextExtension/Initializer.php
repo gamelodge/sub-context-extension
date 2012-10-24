d4047e38452d1e1c5f5614d7ed9aa8d58927eb42
unknown <uqagelde@LIB02GQKDTDJWTW.library.uq.edu.au>
2012-10-24 11:29:10 +1000

    Initial

---
+++ src/UQ/SubContextExtension/Initializer.php
@@ -0,0 +1,59 @@
+<?php
+namespace UQ\SubContextExtension; 
+
+use Behat\Behat\Context\ContextInterface;
+use Behat\Behat\Context\Initializer\InitializerInterface;
+use UQ\SubContextExtension\Context\BusinessSelectorContext;
+
+
+/**
+ * Initializer used by Behat to initialise the BusinessSelector contexts.
+ * 
+ * @author Ben Waine <ben.waine@orange.com>
+ * @author Phill Hicks <phillip.hicks@orange.com>  
+ */
+class Initializer implements InitializerInterface
+{
+    /**
+     * Config Parameters from behat.yml.
+     * 
+     * @var array
+     */
+    private $parameters;
+    
+    /**
+     * Initialises an instance of the Initializer class.
+     * 
+     * @param array $parameters 
+     * 
+     * @return void
+     */
+    public function __construct($parameters)
+    {
+        $this->parameters = $parameters;
+    }
+
+    /**
+     * Informs Behat as to whether the supplied context is supported.
+     * 
+     * @param ContextInterface $context
+     * 
+     * @return Boolean 
+     */
+    public function supports(ContextInterface $context)
+    {
+        return ($context instanceof BusinessSelectorContext);
+    }
+
+    /**
+     * Method passes the parameters to the context.
+     * 
+     * @param ContextInterface $context
+     * 
+     * @return void 
+     */
+    public function initialize(ContextInterface $context)
+    {
+        $context->setParameters($this->parameters);
+    }
+}
